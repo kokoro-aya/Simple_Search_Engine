03338805859661df608aa06cc40df581db3b9034
alicenychto <irol2273@icloud.com>
2020-07-09 12:37:11 +0200

    stage3

--- Simple Search Engine/task/src/search/Main.kt
+++ Simple Search Engine/task/src/search/Main.kt
@@ -1,24 +1,74 @@
 package search
 
+enum class States {
+    MENU, FIND, LIST, EXIT
+}
+
+class Database {
+    private val db = mutableListOf<String>()
+    fun add(content: String) = db.add(content)
+    fun retrieve(pattern: String) = db.filter { it.toLowerCase().contains(pattern.toLowerCase()) }
+    fun retrieveAll() = db
+}
+
+class Program(val db: Database) {
+    var state: States = States.MENU
+
+    fun process() {
+        when (state) {
+            States.MENU -> {
+                try {
+                    println("""
+                    === Menu ===
+                    1. Find a person
+                    2. Print all people
+                    0. Exit
+                """.trimIndent())
+                    state = when (readLine()!!.toInt()) {
+                        0 -> States.EXIT
+                        1 -> States.FIND
+                        2 -> States.LIST
+                        else -> throw IllegalArgumentException()
+                    }
+                } catch (e: IllegalArgumentException) {
+                    println("Incorrect option! Try again.")
+                }
+            }
+            States.FIND -> {
+                println("Enter a name or email to search all suitable people.")
+                val pattern = readLine()!!
+                val result = db.retrieve(pattern)
+                if (result.isEmpty())
+                    println("No matching people found.")
+                else
+                    result.forEach { println(it) }
+                state = States.MENU
+            }
+            States.LIST -> {
+                println("=== List of people ===")
+                db.retrieveAll().forEach { println(it) }
+                state = States.MENU
+            }
+            States.EXIT -> {
+                println("Bye!")
+            }
+        }
+    }
+}
+
+
 fun main() {
     println("Enter the number of people:")
     val size = readLine()!!.toInt()
     println("Enter all people:")
-    val database = mutableListOf<String>()
+    val db = Database()
     for (i in 1 .. size) {
-        database.add(readLine()!!)
+        db.add(readLine()!!)
     }
-    println("Enter the number of search queries:")
-    val queries = readLine()!!.toInt()
-    for (i in 1 .. queries) {
-        println("Enter data to search people:")
-        val data = readLine()!!
-        val result = database.filter { it.toLowerCase().contains(data.toLowerCase()) }
-        if (result.isEmpty()) {
-            println("No matching people found.")
-        } else {
-            println("Found people:")
-            result.forEach { println(it) }
-        }
+    val program = Program(db)
+    while (program.state != States.EXIT) {
+        program.process()
+        if (program.state == States.EXIT)
+            println("Bye!")
     }
 }
